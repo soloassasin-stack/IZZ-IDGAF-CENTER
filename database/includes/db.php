4b02ae9d0e134b12d3252ed49bade916010fd8d0
soloassasin-stack <noorfaiz834@gmail.com>
2026-08-06 23:21:29 +0800

    Update db.php

--- database/includes/db.php
+++ database/includes/db.php
@@ -1,1 +1,23 @@
+<?php
 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "izzidgaf_center";
+
+
+$conn = mysqli_connect(
+    $host,
+    $user,
+    $pass,
+    $db
+);
+
+
+if(!$conn){
+
+die("Database connection failed");
+
+}
+
+?>
