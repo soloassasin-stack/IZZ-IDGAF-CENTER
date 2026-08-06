3b142c14b863b2e11638c413e3573d119de0daec
soloassasin-stack <noorfaiz834@gmail.com>
2026-08-06 23:26:16 +0800

    Update login.php

--- database/login.php
+++ database/login.php
@@ -1,1 +1,62 @@
+<?php
 
+include "includes/db.php";
+
+
+session_start();
+
+
+if(isset($_POST['login'])){
+
+
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+
+$result=mysqli_query($conn,
+
+"SELECT * FROM users WHERE email='$email'");
+
+
+$user=mysqli_fetch_assoc($result);
+
+
+
+if($user && password_verify($password,$user['password'])){
+
+
+$_SESSION['user']=$user['username'];
+
+
+echo "Login Success";
+
+
+}else{
+
+
+echo "Wrong Login";
+
+
+}
+
+
+}
+
+?>
+
+
+<form method="POST">
+
+
+<input name="email" placeholder="Email">
+
+
+<input name="password" type="password" placeholder="Password">
+
+
+<button name="login">
+Login
+</button>
+
+
+</form>
