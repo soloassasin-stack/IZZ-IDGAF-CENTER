c7c802766f5b781d71011a12801e502933b209f9
soloassasin-stack <noorfaiz834@gmail.com>
2026-08-06 23:35:18 +0800

    Update like.php

--- database/like.php
+++ database/like.php
@@ -1,1 +1,18 @@
+<?php
 
+include "includes/db.php";
+
+
+$post_id = $_GET['id'];
+
+
+mysqli_query(
+$conn,
+"INSERT INTO likes(post_id)
+VALUES('$post_id')"
+);
+
+
+header("Location:index.php");
+
+?>
