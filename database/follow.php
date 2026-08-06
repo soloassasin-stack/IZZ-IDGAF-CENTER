e91cdb6bb4c9b6a530d4840dc4499930b476c2b0
soloassasin-stack <noorfaiz834@gmail.com>
2026-08-06 23:44:07 +0800

    Update follow.php

--- database/follow.php
+++ database/follow.php
@@ -1,1 +1,26 @@
+<?php
 
+include "includes/db.php";
+
+session_start();
+
+
+$creator = "IZZ IDGAF";
+
+$follower = $_SESSION['user'] ?? "Guest";
+
+
+mysqli_query(
+$conn,
+
+"INSERT INTO follows(creator,follower)
+
+VALUES('$creator','$follower')"
+
+);
+
+
+header("Location:profile.php");
+
+
+?>
