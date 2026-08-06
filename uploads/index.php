0d04387115f8bf951c8136ca705f869c87c23cb5
soloassasin-stack <noorfaiz834@gmail.com>
2026-08-06 23:31:34 +0800

    Update index.php

--- uploads/index.php
+++ uploads/index.php
@@ -1,1 +1,43 @@
+<?php
 
+include "includes/db.php";
+
+
+$result=mysqli_query(
+$conn,
+"SELECT * FROM posts ORDER BY id DESC"
+);
+
+
+?>
+
+
+<h1>IZZ IDGAF CENTER</h1>
+
+
+<?php while($row=mysqli_fetch_assoc($result)){ ?>
+
+
+<div class="card">
+
+
+<h2>
+<?= $row['title']; ?>
+</h2>
+
+
+<p>
+<?= $row['description']; ?>
+</p>
+
+
+<img 
+src="uploads/<?= $row['image']; ?>"
+width="300"
+>
+
+
+</div>
+
+
+<?php } ?>
