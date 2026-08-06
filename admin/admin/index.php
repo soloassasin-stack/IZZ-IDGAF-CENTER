0cb33421efae413040ad300dd1c0df201795696d
soloassasin-stack <noorfaiz834@gmail.com>
2026-08-06 23:52:55 +0800

    Update index.php

--- admin/admin/index.php
+++ admin/admin/index.php
@@ -1,1 +1,100 @@
+<?php
 
+include "../includes/db.php";
+
+
+$result = mysqli_query(
+$conn,
+"SELECT * FROM posts ORDER BY id DESC"
+);
+
+?>
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<title>
+IZZ IDGAF ADMIN
+</title>
+
+<link rel="stylesheet" href="../assets/style.css">
+
+</head>
+
+
+<body>
+
+
+<header>
+
+<h1>
+IZZ IDGAF ADMIN PANEL
+</h1>
+
+</header>
+
+
+
+<section class="post">
+
+
+<h2>
+Manage Posts
+</h2>
+
+
+
+<?php while($row=mysqli_fetch_assoc($result)){ ?>
+
+
+<div class="card">
+
+
+<h3>
+<?= $row['title']; ?>
+</h3>
+
+
+<p>
+<?= $row['description']; ?>
+</p>
+
+
+<img 
+src="../uploads/<?= $row['image']; ?>"
+width="200"
+>
+
+
+<br><br>
+
+
+<a href="delete.php?id=<?= $row['id']; ?>">
+
+<button>
+Delete
+</button>
+
+</a>
+
+
+
+</div>
+
+
+<br>
+
+
+<?php } ?>
+
+
+</section>
+
+
+</body>
+
+</html>
