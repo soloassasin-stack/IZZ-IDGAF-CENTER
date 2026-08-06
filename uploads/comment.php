cf92cb626d40fdf4e36696d7077304ae7eb5144d
soloassasin-stack <noorfaiz834@gmail.com>
2026-08-06 23:38:54 +0800

    Update comment.php

--- uploads/comment.php
+++ uploads/comment.php
@@ -1,1 +1,30 @@
+<?php
 
+include "includes/db.php";
+
+
+if(isset($_POST['comment'])){
+
+
+$post=$_POST['post_id'];
+
+$text=$_POST['comment'];
+
+
+mysqli_query(
+$conn,
+
+"INSERT INTO comments(post_id,comment)
+
+VALUES('$post','$text')"
+
+);
+
+
+}
+
+
+header("Location:index.php");
+
+
+?>
