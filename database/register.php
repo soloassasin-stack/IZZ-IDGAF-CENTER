345dc1f0d08efc740c35512639ac36618bb3d608
soloassasin-stack <noorfaiz834@gmail.com>
2026-08-06 23:25:47 +0800

    Update register.php

--- database/register.php
+++ database/register.php
@@ -1,1 +1,45 @@
+<?php
 
+include "includes/db.php";
+
+
+if(isset($_POST['register'])){
+
+
+$username=$_POST['username'];
+$email=$_POST['email'];
+$password=password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+
+
+$sql="INSERT INTO users(username,email,password)
+
+VALUES('$username','$email','$password')";
+
+
+mysqli_query($conn,$sql);
+
+
+echo "Account Created!";
+
+}
+
+?>
+
+
+<form method="POST">
+
+
+<input name="username" placeholder="Username">
+
+<input name="email" placeholder="Email">
+
+<input name="password" placeholder="Password" type="password">
+
+
+<button name="register">
+Register
+</button>
+
+
+</form>
